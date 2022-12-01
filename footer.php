456cb2b9ac5a9bf40734025f7237793b5d9c4e3e
Christina <evilelf13@hotmail.com>
2022-12-01 00:18:38 -0800

    experiment with footer animation and add footer menu

--- footer.php
+++ footer.php
@@ -20,8 +20,14 @@
 
 	<footer>
     <div class="footerContainer">
-		  <div class= "row">
+      <div class="waves">
+        <div class="wave" id="wave1"></div>
+        <div class="wave" id="wave2"></div>
+        <div class="wave" id="wave3"></div>
+        <div class="wave" id="wave4"></div>
+      </div>
 
+		  <div class= "row">
         <!--Footer Logo using widget-->
         <div class="footer-logo">
            <?php
@@ -32,10 +38,10 @@
         </div>
 
         <!--Footer Social Media using widget-->
-			  <div class="social-icons">
+			  <div class="footer-menu">
           <?php
-               if(is_active_sidebar('social-media-widget')){
-                dynamic_sidebar('social-media-widget');
+               if(is_active_sidebar('footer-menu-widget')){
+                dynamic_sidebar('footer-menu-widget');
                  }
                ?>
         </div>
